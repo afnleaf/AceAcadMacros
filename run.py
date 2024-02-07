61b61b60d6dc6fe8bb88b763408490b0bb3a08e0
afnleaf <afnleafow@gmail.com>
2024-02-06 23:36:56 -0500

    Version 0.1.0 fixed

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import nxbt
 
 
+'''
 macro_getOutOfMenu = """
 A 0.1s
 2s
@@ -16,8 +17,26 @@ A 0.1s
 2s
 """
 
-
 macro_LoopA = """
+LOOP 100000
+    A 0.1s
+    0.5s
+"""
+'''
+
+macro = """
+A 0.1s
+2s
+B 0.1s
+2s
+DPAD_UP 0.1s
+2s
+DPAD_LEFT 0.1s
+2s
+DPAD_LEFT 0.1s
+2s
+A 0.1s
+2s
 LOOP 100000
     A 0.1s
     0.5s
@@ -29,12 +48,11 @@ nx = nxbt.Nxbt()
 # Create a Pro Controller and wait for it to connect
 controller_index = nx.create_controller(nxbt.PRO_CONTROLLER)
 nx.wait_for_connection(controller_index)
-
 print("connected");
+nx.macro(controller_index, macro)
+
 # Run a macro on the Pro Controller
-nx.macro(controller_index, macro_getOutOfMenu)
-
+# nx.macro(controller_index, macro_getOutOfMenu)
 # Loop A
-nx.macro(controller_index, macro_LoopA)
-
+# nx.macro(controller_index, macro_LoopA)
 # Make a python loop where if you press enter it stops looping?
